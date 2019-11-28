af4f53c1d6d7fc4d74965bb1564a2bf669f24a2c
kristhaj <kristhaj@stud.ntnu.no>
2019-11-28 19:17:41 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Text, View, Header } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}> 
+        <Text style={styles.title}>BREWY</Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.placeholder}>
+          <Text style={styles.placeholderText}>Amizing Brewing Tool To Be Implemented</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#bfc0c0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#2d2d34',
+    alignItems: 'center',
+    width: '100%',
+    height: '20%',
+    padding: 40,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24
+  },
+  content: {
+    flex: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  placeholder: {
+    backgroundColor: '#ef8354',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  placeholderText: {
+    color: '#fff',
+    fontSize: 15,
+  }
+});

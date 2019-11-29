914a79b0e07f543b70ff8259b3085c76591c896c
kristhaj <kristhaj@stud.ntnu.no>
2019-11-29 21:27:34 +0100

    make fetch async

--- Stores/RecipeStore.js
+++ Stores/RecipeStore.js
@@ -1,30 +1,40 @@
-import { observable , action} from 'mobx';
+import { observable , action, runInAction} from 'mobx';
 
 class RecipeStore {
     @observable recipes = [];
     @observable recipeIDList = [];
     @observable URL;
+    @observable state = "pending"; // "pending" / "done" / "error"
 
     constructor() {
         //api endpoint for getting a single recipe
         this.URL = "https://api.brewersfriend.com/v1/recipes/";
     }
 
-    @action fetchRecipeDataAsync = (recipeIDindex) => {
+    @action
+    async fetchRecipeDataAsync(recipeIDindex) {
         //TODO make a store for API keys so anyone can use it with their personal key, and not rely upon this one
         const apiKey = "cd138a938274f356b6e0f9d28fb91dedae6a35eb";
         const urlParam = this.recipeIDList[recipeIDindex];
+        this.state = "pending";
 
         try {
             //TODO push the desired data from the response into recipes list
             const data = await getData(this.URL + urlParam, apiKey)
             console.log(JSON.stringify(data));
+            runInAction(() => {
+                this.state = "done";
+                this.recipes.push(data);
+            });
         } catch(e) {
-            console.error("HTTP error: " + e);
+            runInAction(() => {
+                this.state = "error";
+                console.error("HTTP error: " + e);
+            });
         }
     }
 
-    getData = (url, key) => {
+    async getData(url, key) {
         //simple GET request for one single recipe
         const response = await fetch(url+urlParam, {
             method: "GET",

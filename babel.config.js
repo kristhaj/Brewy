d73fdfcba87c65522d47972a031a42505d042849
kristhaj <kristhaj@stud.ntnu.no>
2020-01-23 13:13:14 +0100

    solve error due to incorrect babel config

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      "@babel/preset-env",
+      "babel-preset-expo",
       "module:metro-react-native-babel-preset"
     ],
     plugins: [
-      [
-        "@babel/plugin-proposal-decorators",
-        {
-          "legacy": true
-        }
-      ], [
-        "@babel/plugin-proposal-object-rest-spread"
-      ]
+      
+        ["@babel/plugin-proposal-decorators", {"legacy": true}]
       
     ]
-  };
+
+  }
 };
